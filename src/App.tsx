f72573599be09a2c21b3376cbf3390b0856adeef
njmoney21 <nikolatodorovic800@gmail.com>
2026-05-16 01:01:40 +0200

    feat: assemble full Klarvis website

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import Navbar from './components/Navbar'
+import SectionHero from './components/SectionHero'
+import SectionLeistungen from './components/SectionLeistungen'
+import SectionProcess from './components/SectionProcess'
+import SectionPortfolio from './components/SectionPortfolio'
+import SectionPreise from './components/SectionPreise'
+import SectionKontakt from './components/SectionKontakt'
+import Footer from './components/Footer'
+
 export default function App() {
-  return <div>Klarvis</div>
+  return (
+    <>
+      <Navbar />
+      <SectionHero />
+      <SectionLeistungen />
+      <SectionProcess />
+      <SectionPortfolio />
+      <SectionPreise />
+      <SectionKontakt />
+      <Footer />
+    </>
+  )
 }
